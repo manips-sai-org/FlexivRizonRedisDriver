4f2410fcc41c875002f92d3defedb6c34ede907e
Wesley Guo <wesleyyguo@gmail.com>
2025-11-15 16:00:21 -0800

    Edited Config Files to work for srl-titania computer which has cores 4 through 7 isolated to reserve them for robot driver and flexiv scheduler

--- redis_driver/launch_oberon-4s_gripper_driver.sh
+++ redis_driver/launch_oberon-4s_gripper_driver.sh
@@ -6,5 +6,5 @@ sudo cpufreq-set -c 3 -g performance
 sudo cpupower -c 3 frequency-set -d 4200MHz 
 sudo cpufreq-set -c 4 -g performance
 sudo cpupower -c 4 frequency-set -d 4200MHz 
-sudo taskset --cpu-list 4 chrt -rr 80 ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
+sudo taskset --cpu-list 5 chrt -rr 80 ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
 # ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
--- redis_driver/launch_titania-4s_gripper_driver.sh
+++ redis_driver/launch_titania-4s_gripper_driver.sh
@@ -6,5 +6,5 @@ sudo cpufreq-set -c 3 -g performance
 sudo cpupower -c 3 frequency-set -d 4200MHz 
 sudo cpufreq-set -c 4 -g performance
 sudo cpupower -c 4 frequency-set -d 4200MHz 
-sudo taskset --cpu-list 5 chrt -rr 79 ./build/flexiv_rizon4_redis_driver_with_gripper config_titania.xml
+sudo taskset --cpu-list 7 chrt -rr 79 ./build/flexiv_rizon4_redis_driver_with_gripper config_titania.xml
 # ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
