5e4fd319604553c6c6efc67a03f6d22e2bbb9c55
Wesley Guo <wesleyyguo@gmail.com>
2025-11-26 17:46:33 -0800

    Minor driver tweaks to gripper control, minor specification changes. Version used in onsite testing at 11-26-25

--- redis_driver/launch_claudio-4s_gripper_driver.sh
+++ redis_driver/launch_claudio-4s_gripper_driver.sh
@@ -1,7 +1,7 @@
-sudo cpufreq-set -c 4 -g performance
-sudo cpupower -c 4 frequency-set -d 4200MHz 
-sudo cpufreq-set -c 5 -g performance
-sudo cpupower -c 5 frequency-set -d 4200MHz 
-# sudo taskset --cpu-list 5 chrt -rr 80 ./build/flexiv_rizon4_redis_driver_with_gripper config_hero.xml
-sudo taskset --cpu-list 5 chrt -rr 80 ./build/flexiv_rizon4_redis_driver_robot_only config_claudio.xml
+sudo cpufreq-set -c 2 -g performance
+sudo cpupower -c 2 frequency-set -d 4200MHz 
+sudo cpufreq-set -c 3 -g performance
+sudo cpupower -c 3 frequency-set -d 4200MHz 
+sudo taskset --cpu-list 3 chrt -f 90 ./build/flexiv_rizon4_redis_driver_with_gripper config_claudio.xml
+# sudo taskset --cpu-list 3 chrt -rr 80 ./build/flexiv_rizon4_redis_driver_robot_only config_beatrice.xml
 # ./build/flexiv_rizon4_redis_driver_with_gripper config_hero.xml
