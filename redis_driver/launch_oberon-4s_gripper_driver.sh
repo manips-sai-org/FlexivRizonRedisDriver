2ebc6e81c5ce0b78ec4405b06e3a0cdf9811c573
Wesley Guo <wesleyyguo@gmail.com>
2026-06-09 22:53:31 -0700

    Made updates to CPU affinity, frequency, and IP address to conform to SRC datacenter setup with original srl-titania computer

--- redis_driver/launch_oberon-4s_gripper_driver.sh
+++ redis_driver/launch_oberon-4s_gripper_driver.sh
@@ -1,6 +1,6 @@
 sudo cpufreq-set -c 2 -g performance
-sudo cpupower -c 2 frequency-set -d 4200MHz 
+sudo cpupower -c 2 frequency-set -d 5000MHz 
 sudo cpufreq-set -c 3 -g performance
-sudo cpupower -c 3 frequency-set -d 4200MHz 
-sudo taskset --cpu-list 3 chrt -f 90 ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
+sudo cpupower -c 3 frequency-set -d 5000MHz 
+sudo taskset --cpu-list 2 chrt -f 90 ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
 # ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
