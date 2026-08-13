f4ef103cfb04d3d9f316206e2769482344230238
Wesley Guo <wesleyyguo@gmail.com>
2026-08-13 16:16:16 -0700

    Minor change to CPU frequency for oberon

--- redis_driver/launch_oberon-4s_gripper_driver.sh
+++ redis_driver/launch_oberon-4s_gripper_driver.sh
@@ -2,5 +2,7 @@ sudo cpufreq-set -c 2 -g performance
 sudo cpupower -c 2 frequency-set -d 5000MHz 
 sudo cpufreq-set -c 3 -g performance
 sudo cpupower -c 3 frequency-set -d 5000MHz 
+sudo cpufreq-set -c 4 -g performance
+sudo cpupower -c 4 frequency-set -d 5000MHz 
 sudo taskset --cpu-list 2 chrt -f 90 ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
 # ./build/flexiv_rizon4_redis_driver_with_gripper config_oberon.xml
